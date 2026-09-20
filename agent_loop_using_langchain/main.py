aaff84d56403a5f2765f72d055798cd5c10d3139
Nabin Prasad Dhungana <prnabin123@gmail.com>
2026-09-20 09:39:30 +0545

    Removed unused json module import and ran the calling and result flow only if ran directly

--- agent_loop_using_langchain/main.py
+++ agent_loop_using_langchain/main.py
@@ -1,5 +1,4 @@
 import os
-import json
 from dotenv import load_dotenv
 
 from langchain_openai import ChatOpenAI
@@ -16,70 +15,70 @@ model = ChatOpenAI(
     api_key=os.getenv("OPENROUTER_API_KEY"),
 )
 
-
-@tool
-def get_customer(customer_id: int):
-    """Get customer information by customer ID."""
-    
-    customers = {
-        1: {
-            "name": "Ram",
-            "email": "ram@example.com"
-        },
-        2: {
-            "name": "Sita",
-            "email": "sita@example.com"
+if __name__ == '__main__':
+    @tool
+    def get_customer(customer_id: int):
+        """Get customer information by customer ID."""
+        
+        customers = {
+            1: {
+                "name": "Ram",
+                "email": "ram@example.com"
+            },
+            2: {
+                "name": "Sita",
+                "email": "sita@example.com"
+            }
         }
+
+        return customers.get(customer_id)
+
+    @tool
+    def calculate_total(quantity: float, price: float):
+        """Get total price by quantity and price."""
+        return quantity * price
+
+    tool_registry = {
+        "get_customer": get_customer,
+        "calculate_total": calculate_total,
     }
 
-    return customers.get(customer_id)
+    model_with_tools = model.bind_tools([get_customer, calculate_total])
 
-@tool
-def calculate_total(quantity: float, price: float):
-    """Get total price by quantity and price."""
-    return quantity * price
+    messages = [
+        SystemMessage(content="You are a helpful assistant."),
+        HumanMessage(content="Calculate the total for 8 items costing 35 each."),
+    ]
 
-tool_registry = {
-    "get_customer": get_customer,
-    "calculate_total": calculate_total,
-}
+    response = model_with_tools.invoke(messages)
 
-model_with_tools = model.bind_tools([get_customer, calculate_total])
+    print(response.content)
+    print("---------------------------")
+    print(response.tool_calls)
 
-messages = [
-    SystemMessage(content="You are a helpful assistant."),
-    HumanMessage(content="Calculate the total for 8 items costing 35 each."),
-]
+    tools = response.tool_calls
 
-response = model_with_tools.invoke(messages)
-
-print(response.content)
-print("---------------------------")
-print(response.tool_calls)
-
-tools = response.tool_calls
-
-if tools:
-    for tool_call in tools:
-        tool_name = tool_call["name"]
-        arguments = tool_call["args"]
-        
-        if tool_name not in tool_registry:
-            print(f"Unknown tool: {tool_name}")
-            continue
-        
-        result = tool_registry[tool_name].invoke(arguments)
-        
-        tool_message = {
-            "content": result,
-            "tool_call_id": tool_call["id"],
-        }
-        messages.append(ToolMessage(**tool_message))
-             
+    if tools:
+        for tool_call in tools:
+            tool_name = tool_call["name"]
+            arguments = tool_call["args"]
             
-        print(f"Tool call result for tool {tool_name} = {result}")
-        
-final_answer = model_with_tools.invoke(messages)
+            if tool_name not in tool_registry:
+                print(f"Unknown tool: {tool_name}")
+                continue
+            
+            result = tool_registry[tool_name].invoke(arguments)
+            
+            tool_message = {
+                "content": result,
+                "tool_call_id": tool_call["id"],
+            }
+            messages.append(ToolMessage(**tool_message))
+                
+                
+            print(f"Tool call result for tool {tool_name} = {result}")
+            
+    final_answer = model_with_tools.invoke(messages)
 
-print("---------------------")
-print(final_answer.content)
+    print("---------------------")
+    print(final_answer.content)
